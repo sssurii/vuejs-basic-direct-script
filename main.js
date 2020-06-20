b7dc82418f7b9673aab2f79d8da5b93416e4a1cd
Surinder Singh <sssurii.dev@gmail.com>
2020-06-20 20:26:05 +0530

    rendering a list in Vuejs - for loop in vuejs

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const app = new Vue({
     el: '#app',
     data: {
-        project:'Vue.js'
+        project: 'Vue.js'
+    },
+    created() {
+        this.list = {
+            0: "Learn Basic Concept - How Vue.js works",
+            1: "Setup Vue.js",
+            2: "Learn Vue.js syntax",
+            3: "Hello world",
+            4: "First dummy component"
+        }
     }
 });
